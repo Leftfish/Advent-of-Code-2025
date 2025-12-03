85082500f9085ed6e265fd9557545025986dbc4f
Leftfish <bohdan@widla.net>
2025-12-03 11:03:48 +0100

    Add full solution to Day 3.

--- 03/d03.py
+++ 03/d03.py
@@ -4,6 +4,7 @@ import os
 def parse_data(data):
     return [[int(i) for i in row] for row in data.splitlines()]
 
+
 def find_best(row):
     best = 0
     idx = 0
@@ -13,10 +14,56 @@ def find_best(row):
             idx = i
     return best, idx
 
+
 def check_row(row):
     first, idx = find_best(row[:len(row)-1])
     second, _ = find_best(row[idx+1:])
     return 10* first + second
+
+
+def get_k_best_digits(row, k):
+    n = len(row)
+    dp = {}
+
+    def dfs(idx, remaining):
+        # Base case: no more digits to add, so we add nothing more
+        if remaining == 0:
+            return ''
+        # Base case: we reached the end of the row without building a k-digit number
+        if idx == n:
+            return None
+        # Base case: we do not have enough digits left in the row to build a k-digit number
+        if (n - idx) < remaining:
+            return None
+        # DP: we have already been there (at idx p with q digits to go) so pick the state from the cache
+        if (idx, remaining) in dp:
+            return dp[(idx, remaining)]
+
+
+        # Take/skip approach: we try either append the next digit to the result and move forward
+        # (take_result if it does not lead to a dead-end base case) or just move forward (skip_result)
+        take_next = dfs(idx+1, remaining-1)
+
+        if take_next is not None:
+            take_result = row[idx] + take_next
+        else:
+            take_result = None
+
+        skip_result = dfs(idx+1, remaining)
+
+        # Then we move forward with the better option, saving it in the cache
+        if take_result is None:
+            best = skip_result
+        elif skip_result is None:
+            best = take_result
+        else:
+            best = max(take_result, skip_result)
+
+        dp[(idx, remaining)] = best
+        return best
+
+    return dfs(0, k)
+
 
 DAY = 3
 TEST_DATA = '''987654321111111
@@ -28,11 +75,15 @@ TEST_DATA = '''987654321111111
 print(f'Day {DAY} of Advent of Code!')
 print('Testing...')
 rows = parse_data(TEST_DATA)
-print('Best joltages from left to right:', sum(check_row(r) for r in rows) == 357)
-
+print('Best joltages from left to right (2 batteries):', sum(check_row(r) for r in rows) == 357)
+print('Best joltages from left to right (2 batteries recursive):', sum(int(get_k_best_digits(r, 2)) for r in TEST_DATA.splitlines()) == 357)
+print('Best joltages from left to right (12 batteries recursive):', sum(int(get_k_best_digits(r, 12)) for r in TEST_DATA.splitlines()) == 3121910778619)
 
 input_path = f"{os.getcwd()}\\{str(DAY).zfill(2)}\\inp"
 with open(input_path, mode='r', encoding='utf-8') as inp:
     print('Solution...')
-    rows = parse_data(inp.read())
-    print('Best joltages from left to right:', sum(check_row(r) for r in rows))
+    data = inp.read()
+    rows = parse_data(data)
+    print('Best joltages from left to right (2 batteries):', sum(check_row(r) for r in rows))
+    print('Best joltages from left to right (2 batteries recursive):', sum(int(get_k_best_digits(r, 2)) for r in data.splitlines()))
+    print('Best joltages from left to right (12 batteries recursive):', sum(int(get_k_best_digits(r, 12)) for r in data.splitlines()))
--- README.md
+++ README.md
@@ -9,9 +9,10 @@ For the eight time I'm going to try to find out how far I can make it in [Advent
 * [2023](https://github.com/Leftfish/Advent-of-Code-2023): 25 days for the fourth time!
 * [2024](https://github.com/Leftfish/Advent-of-Code-2024): 25 days for the fifth time!
 
-For years I've been telling myself that I've had enough, that I no longer have time, that I have other stuff to do. So far, AoC won with everything, although ususally I had a couple of stars left to collect at the end of the regular AoC schedule. We'll see how it goes this time. It's going to be only 12 days, so I anticipate the difficulty to rise far quicker.
+For years I've been telling myself that I've had enough, that I no longer have time, that I have other stuff to do. So far, AoC won with everything, although ususally I had a couple of stars left to collect at the end of the regular AoC schedule. We'll see how it goes this time. It's going to be only 12 days, so I anticipate the difficulty to rise far quicker. As of day 3, I had the first big challenge (the second part required dynamic programming, which is one of my weaker skills).
 
 Things I **L**earned, **R**evised or **I**mproved at in 2025:
 
 * [Day 1 Python](01/d01.py): modulo arithmetic (**R**)
 * [Day 2 Python](02/d02.py): using log10 to count the number of digits (**R**)
+* [Day 3 Python](03/d03.py): DFS (**R**), dynamic programming (**L**/**I**)
