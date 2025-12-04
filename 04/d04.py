69e81ca62fab676ce9e1161454ff39f3cb12c9df
Leftfish <bohdan@widla.net>
2025-12-04 18:25:01 +0100

    Fix var names.

--- 04/d04.py
+++ 04/d04.py
@@ -34,7 +34,7 @@ def find_removable(grid):
     return {coord for coord in grid if grid[coord] == PAPER and accessible(find_adjacent(grid, coord))}
 
 
-def count_with_pseudo_to_checkueue(grid):
+def count_with_set(grid):
     s = 0
     to_check = find_removable(grid)
 
@@ -68,7 +68,7 @@ print(f'Day {DAY} of Advent of Code!')
 print('Testing...')
 warehouse = parse_data(TEST_DATA)
 print('Initially removable:', len(find_removable(warehouse)) == 13)
-print('Iteratively removed (set as pseudo-to_checkueue):', count_with_pseudo_to_checkueue(warehouse) == 43)
+print('Iteratively removed (set as pseudo-to_checkueue):', count_with_set(warehouse) == 43)
 
 
 input_path = f"{os.getcwd()}\\{str(DAY).zfill(2)}\\inp"
@@ -77,4 +77,4 @@ with open(input_path, mode='r', encoding='utf-8') as inp:
     data = inp.read()
     warehouse = parse_data(data)
     print('Initially removable:', len(find_removable(warehouse)))
-    print('Iteratively removed (set as pseudo-to_checkueue):', count_with_pseudo_to_checkueue(warehouse))
+    print('Iteratively removed (set as pseudo-to_checkueue):', count_with_set(warehouse))
